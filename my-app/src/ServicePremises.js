41cde7bbf6a87faa097e35889ec0413af3b1f54d
RufatRahmatov <rehmetovruft@gmail.com>
2024-07-17 05:45:40 -0700

    Feature: Add Responsive

--- my-app/src/ServicePremises.js
+++ my-app/src/ServicePremises.js
@@ -7,32 +7,32 @@ const ServicePremises = () => {
         {
             icon: <FaBuilding />,
             title: 'Service Premises',
-            text: 'Our complex has office space and convenience stores on the ground floor.'
+            text: 'Our complex has office space and convenience stores on the ground floor.Theres lot of hate out there for a text very big bulding.'
         },
         {
             icon: <FaShieldAlt />,
             title: 'Safety and Security',
-            text: 'Reliable round-the-clock surveillance by a security system using modern technologies.'
+            text: 'Reliable round-the-clock surveillance by a security system using modern technologies. The villages are out there with'
         },
         {
             icon: <FaLeaf />,
             title: 'Environmental Solutions',
-            text: 'We care about the environment, using LED lighting and having a charging station for electric vehicles.'
+            text: 'We care about the environment, using LED lighting and having a charging station for electric vehicles clinets.'
         },
         {
             icon: <FaToolbox />,
             title: 'Ease of Management',
-            text: 'You just buy an apartment, and a professional hotel operator will do the rest for you.'
+            text: 'You just buy an apartment, and a professional hotel operator will do the rest for you.That Amounts to little more than garbled words'
         },
         {
             icon: <FaMoneyCheckAlt />,
             title: 'Guaranteed Income',
-            text: 'Guaranteed monthly income is prescribed in advance in the contract.'
+            text: 'Guaranteed monthly income is prescribed in advance in the contract.True enough but that not all that it takes to get things'
         },
         {
             icon: <FaMapMarkerAlt />,
             title: 'Great Location',
-            text: 'A pledge of a secure rental of your apartment.'
+            text: 'A pledge of a secure rental of your apartment.A client that unhapy for a reason is a problem,acliient thats unhapy.'
         }
     ];
 
